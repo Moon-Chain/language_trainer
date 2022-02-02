f2cffcb36699e8da171d374bb63f11fac4ceba72
moonchain <berkayolce@gmail.com>
2022-02-02 19:36:57 +0300

    initial

---
+++ assets/js/english_words.js
@@ -0,0 +1,28 @@
+language_list = [];
+lang = {
+    en_word: "and",
+    en_alt: "",
+    tr_word: "ve",
+    tr_alt: "falan filan",
+};
+
+language_list.push(lang);
+
+lang = {
+    en_word: "use",
+    en_alt: "",
+    tr_word: "kullan",
+    tr_alt: "",
+};
+
+language_list.push(lang);
+
+
+lang = {
+    en_word: "do",
+    en_alt: "just do it",
+    tr_word: "yapmak",
+    tr_alt: "yapmak anlamına gelir",
+};
+
+language_list.push(lang);
